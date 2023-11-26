ab51738f726952ef4dbe44ac6bc7ec7241995cb9
Mos <mosrichard3@gmail.com>
2023-11-27 00:08:51 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
 import "./App.css";
 import Login from "./components/Login";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="app">
       <Router>
         <Routes>
-          <Route exact path="/" Component={Login}></Route>
+          <Route path="/" element={<Login />} />
+          <Route path="/Home" element={<React.Fragment><Header /><Home /></React.Fragment>} />
         </Routes>
       </Router>
     </div>
